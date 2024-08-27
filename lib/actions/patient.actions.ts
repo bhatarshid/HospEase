62354fb27c6b50ab7b01ab90da7871f4b9765453
bhatarshid <bhatarshid1000@gmail.com>
2024-08-27 17:32:29 +0530

    registration page

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,9 +1,13 @@
+"use server"
+
 import { ID, Query } from 'node-appwrite';
-import { users } from '../appwrite.config';
+import { databases, storage, users } from '../appwrite.config';
+import { parseStringify } from '../utils';
+
+import { InputFile } from 'node-appwrite/file'
 
 export const createUser = async (user: CreateUserParams) => {
   try {
-    console.log('lkadjfla sdflkjkasdflkj ')
     const newuser = await users.create(
       ID.unique(),
       user.email,
@@ -11,7 +15,6 @@ export const createUser = async (user: CreateUserParams) => {
       undefined,
       user.name,
     );
-    console.log("new user is", newuser)
     return newuser;
   }
   catch (error: any) {
@@ -23,5 +26,48 @@ export const createUser = async (user: CreateUserParams) => {
 
       return existingUser?.users[0];
     }
+  }
+}
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    
+    return parseStringify(user)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+export const registerPatient = async ({ identificationDocument, ...patient }: RegisterUserParams) => {
+  try{
+    let file;
+
+    if(identificationDocument){
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get('blobFile') as Blob,
+        identificationDocument?.get('fileName') as string
+      );
+
+      file = await storage.createFile(process.env.NEXT_PUBLIC_BUCKET_ID!, ID.unique(), inputFile)
+      
+    }
+
+    const newPatient  = await databases.createDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${process.env.NEXT_PUBLIC_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_BUCKET_ID}/files/${file?.$id}/view?project=${process.env.NEXT_PUBLIC_PROJECT_ID}`, 
+        ...patient 
+      }
+    )
+
+    return parseStringify(newPatient);
+  }
+  catch (error) {
+    console.log(error);
   }
 }
