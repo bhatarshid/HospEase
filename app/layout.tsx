d09079ff6da3313920758490407777543542c238
bhatarshid <bhatarshid1000@gmail.com>
2024-09-07 00:09:24 +0530

    new setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Lexend } from "@next/font/google";
+import "./globals.css";
+
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"]
+});
+
+export const metadata: Metadata = {
+  title: "HospEase",
+  description: "A health care managemet system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${lexend.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
