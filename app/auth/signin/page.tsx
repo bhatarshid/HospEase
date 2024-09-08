1938ca6a5b2026ce458b6123fecb75575865c500
bhatarshid <bhatarshid1000@gmail.com>
2024-09-09 00:05:13 +0530

    login and signup design

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,43 @@
+import SigninForm from "@/Components/forms/SigninForm"
+import Image from "next/image"
+import Link from "next/link"
+
+const Signin = () => {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="container my-auto">
+        <div className="mx-auto flex size-full flex-col max-w-[496px]">
+          <Image 
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit bg-primary mt-5"
+          />
+        </div>
+        
+        <section className="text-[#6b6969]">
+          <h1 className="text-[28px] text-center flex justify-center">Login</h1>
+          <p className="text-[14px] font-thin flex justify-center">Sign in to continue</p>
+        </section>
+        <hr className="my-5"/>
+
+        <SigninForm />
+
+        <div className="flex flex-row justify-between">
+          <p className="copyright py-12 font-thin text-sm">© 2024 CarePluse</p>
+          <p className="copyright py-12 font-thin text-sm">Don&apos;t have an account? <Link href="/auth/signup" className="text-blue-700">Signup</Link></p>
+        </div>
+      </section>
+      <Image 
+        src="/assets/images/doctor-image.png"
+        alt="doctor"
+        width={1000}
+        height={1000}
+        className="max-w-[50%]"
+      />
+    </div>
+  )
+}
+
+export default Signin
