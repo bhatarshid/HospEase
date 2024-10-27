e07dd50e4a9aa83fc91cec0ff42656d11adb9111
bhatarshid <bhatarshid1000@gmail.com>
2024-10-27 18:27:01 +0530

    appointment booking

--- Components/AppointmentModal.tsx
+++ Components/AppointmentModal.tsx
@@ -20,9 +20,12 @@ import CustomFormField, { FormFieldType } from "./CustomFormField";
 import { Button } from "./ui/button";
 import SubmitButton from "./SubmitButton";
 import { useEffect, useState } from "react";
-import { formatDate, formatTimeSlot } from "@/lib/utils";
+import { formatDate, formatEndTimeSlot, formatTimeSlot } from "@/lib/utils";
 import { ServiceDoctorDetails } from "@/types/entities/service-types";
-
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { toast } from "react-toastify";
+import { bookAppointment, reset } from "@/redux/features/service-slice";
 
 export const AppointmentModal = ({
   isOpen, 
@@ -35,14 +38,24 @@ export const AppointmentModal = ({
 }) => { 
   const [availableDates, setAvailableDates] = useState<string[]>([]);
   const [availableTimeSlots, setAvailableTimeSlots] = useState<string[]>([]);
+  const [formInitialized, setFormInitialized] = useState(false);
+  const { isError, isSuccess, isLoading, message} = useSelector((state: RootState) => state.service);
+  const {user} = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const form = useForm();
   
-  const form = useForm({
-    defaultValues: {
-      selectedDate: '',
-      selectedTime: '',
-      reason: ''
+  useEffect(() => {
+    if(doctorData && !formInitialized) {
+      form.reset({
+        serviceDoctorId: doctorData?.id,
+        selectedDate: '',
+        selectedTime: '',
+        reason: ''
+      });
+      setFormInitialized(true);
     }
-  })
+  }, [doctorData, formInitialized, form]);
 
   useEffect(() => {
     if(doctorData?.slots) {
@@ -50,6 +63,21 @@ export const AppointmentModal = ({
       setAvailableDates(dates);
     }
   }, [doctorData])
+
+  useEffect(() => {
+    if(isError) {
+      toast.error("Failed to book an appointment")
+      dispatch(reset())
+    }
+
+    if(isSuccess && doctorData) {
+      toast.success("Appointment booked successfully")
+      dispatch(reset())
+      onClose()
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, dispatch]);
 
   const handleDateChange = (date: string) => {
     form.setValue('selectedDate', date);
@@ -60,7 +88,27 @@ export const AppointmentModal = ({
   }
 
   const onSubmit = async (data: any) => {
-    console.log(data)
+    if (data.selectedDate === '' || data.selectedTime === '') {
+      toast.error("Please select a date and time");
+      return;
+    }
+
+    if (data.reason === '') {
+      toast.error("Please provide a reason for the appointment");
+      return;
+    }
+
+    const appointmentDate = new Date(data.selectedDate +' '+ data.selectedTime).toISOString();
+    const requestBody: {
+      serviceDoctorId: string;
+      appointmentDate: string;
+      reason: string;
+    } = {
+      serviceDoctorId: data.serviceDoctorId,
+      appointmentDate,
+      reason: data.reason
+    }
+    dispatch(bookAppointment(requestBody));    
   }
   
   return (
@@ -84,7 +132,7 @@ export const AppointmentModal = ({
             <div>
               <Label className="shad-input-label">Patient Name</Label>
               <p className="mt-1 text-dark1 bg-[#ebe9e9] p-2 border rounded-[6px] font-normal">
-                FirstName LastName
+                {user?.firstName} {user?.lastName}
               </p>
             </div>
             <div className="grid grid-cols-2 gap-4">
@@ -98,8 +146,8 @@ export const AppointmentModal = ({
                     <SelectValue placeholder="Select date" />
                   </SelectTrigger>
                   <SelectContent className="bg-white">
-                    {availableDates.map((date) => (
-                      <SelectItem key={date} value={date}>
+                    {availableDates.map((date, index) => (
+                      <SelectItem key={index} value={date}>
                         {formatDate(date)}
                       </SelectItem>
                     ))}
@@ -119,8 +167,8 @@ export const AppointmentModal = ({
                   </SelectTrigger>
                   <SelectContent className="bg-white">
                     {availableTimeSlots.map((time, index) => (
-                      <SelectItem key={index} value={time}>
-                        {formatTimeSlot(time)}
+                      <SelectItem key={index} value={formatTimeSlot(time)}>
+                        {formatTimeSlot(time)} - {formatEndTimeSlot(time)}
                       </SelectItem>
                     ))}
                   </SelectContent>
@@ -136,7 +184,7 @@ export const AppointmentModal = ({
             />
 
             <div className="flex gap-4 pt-4 w-1/2">
-              <SubmitButton isLoading={false}>
+              <SubmitButton isLoading={isLoading}>
                 Book Appointment
               </SubmitButton>
               <Button
