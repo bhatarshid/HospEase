5354484737f02440e2e84e3bb50ca912a8d8959d
bhatarshid <bhatarshid1000@gmail.com>
2025-01-26 23:34:22 +0530

    start over

--- redux/store.ts
+++ redux/store.ts
@@ -1,15 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth-slice';
-import doctorReducer from './features/doctor-slice';
-import profileReducer from './features/profile-slice';
-import serviceReducer from './features/service-slice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    doctor: doctorReducer,
-    profile: profileReducer,
-    service: serviceReducer
   },    // function takes current state, action and returns new state
 });
 
